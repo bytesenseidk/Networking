07655b30abd3cd969b854f4620011a425892d853
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-03 18:05:08 +0200

    Update NetworkScan.py

--- NetworkScan.py
+++ NetworkScan.py
@@ -3,46 +3,27 @@ from tabulate import tabulate
 
 class Network_Details(object):
     def __init__(self):
-        self.interfaces    = []
-        self.address_ip    = []
-        self.netmask_ip    = []
-        self.broadcast_ip  = []
-        self.address_mac   = []
-        self.netmask_mac   = []
-        self.broadcast_mac = []
         self.parser = psutil.net_if_addrs()
-        self.scanner()
 
     def __repr__(self):
-        data = {"Interface": [*self.active_interfaces()],
-                "IP-Address": [*self.address_ip],
-                "MAC-Address": [*self.address_mac],  
-                "Netmask_IP": [*self.netmask_ip],
-                "Netmask_MAC": [*self.netmask_mac],
-                "Broadcast-IP": [*self.broadcast_ip],
-                "Broadcast-MAC": [*self.broadcast_mac]
-                }
-        return tabulate(data, headers="keys", tablefmt="github")
-
-    def active_interfaces(self):
-        unique = []
-        for interface in self.interfaces:
-            if interface not in unique:
-                unique.append(interface)
-        return unique
-    
-    def scanner(self):
+        self.interfaces = []
+        self.address_ip = []
+        self.netmask_ip = []
+        self.broadcast_ip = []
         for interface_name, interface_addresses in self.parser.items():
+            self.interfaces.append(interface_name)
             for address in interface_addresses:
-                self.interfaces.append(interface_name)
                 if str(address.family) == 'AddressFamily.AF_INET':
                     self.address_ip.append(address.address)
                     self.netmask_ip.append(address.netmask)
                     self.broadcast_ip.append(address.broadcast)
-                elif str(address.family) == 'AddressFamily.AF_PACKET':
-                    self.address_mac.append(address.address)
-                    self.netmask_mac.append(address.netmask)
-                    self.broadcast_mac.append(address.broadcast)
+        data = {"Interface"    : [*self.interfaces],
+                "IP-Address"   : [*self.address_ip],  
+                "Netmask"      : [*self.netmask_ip],
+                "Broadcast-IP" : [*self.netmask_ip]
+                }
+        return tabulate(data, headers="keys", tablefmt="github")
 
 if __name__ == "__main__":
     print(Network_Details())
+
