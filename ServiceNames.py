8f9e9ce461295dc083bad4bcf911fb94cc79eece
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-22 15:31:37 +0200

    Update ServiceNames.py

--- ServiceNames.py
+++ ServiceNames.py
@@ -1,13 +1,13 @@
 import socket
 
+
 def find_service_name():
-    ports = [80, 25]
+    ports = [80, 25, 22, 990]
     for port in ports: 
         print(f"Port: {port} >> Service Name: " 
               f"{socket.getservbyport(port, 'tcp')}")
 
-        print(f"Port: {53} >> Service Name: "
-              f"{socket.getservbyport(53, 'udp')}")
 
 if __name__ == "__main__":
     find_service_name()
+
