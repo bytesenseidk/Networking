90710d42d50b12008ed7519b232e47cee248368d
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-05-05 12:05:10 +0200

    Add files via upload

--- NetworkSpeed.py
+++ NetworkSpeed.py
@@ -4,19 +4,21 @@ from tabulate import tabulate
 
 class Network_Details(object):
     def __init__(self):
-        self.parser = psutil.net_if_addrs()
-        self.speed_parser = speedtest.Speedtest()
+        self.scanner = psutil.net_if_addrs()
+        self.speed = speedtest.Speedtest()
         self.interfaces = self.interface()[0]
+
 
     def interface(self):
         interfaces = []
-        for interface_name, _ in self.parser.items():
+        for interface_name, _ in self.scanner.items():
             interfaces.append(str(interface_name))
         return interfaces
-    
-    def __repr__(self):
-        down = str(f"{round(self.speed_parser.download() / 1_000_000, 2)} Mbps")
-        up = str(f"{round(self.speed_parser.upload() / 1_000_000, 2)} Mbps")
+
+
+    def test(self):
+        down = str(f"{round(self.speed.download() / 1_000_000, 2)} Mbps")
+        up = str(f"{round(self.speed.upload() / 1_000_000, 2)} Mbps")
         interface = self.interfaces
         data = {"Interface:" : [interface],
                 "Download:" : [down],
@@ -24,6 +26,10 @@ class Network_Details(object):
         table = tabulate(data, headers="keys", tablefmt="pretty")
         return table
 
+
+    def __str__(self):
+        return str(self.test())
+
+
 if __name__ == "__main__":
     print(Network_Details())
-
