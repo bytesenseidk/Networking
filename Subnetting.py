8949cb9469abd222c3f715423dbf9eb6fd7513bd
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-10 19:33:16 +0200

    Subnetting done

--- Subnetting.py
+++ Subnetting.py
@@ -12,31 +12,33 @@ class Subnet(object):
             "8": [32, 27], "16": [16, 28], "32": [8, 29], 
             "64": [4, 30], "128": [2, 31], "256": [1, 32]
         }
-    
-    # def __str__(self):
-    #     specs = ["subnet", "host", "snm"]
-    #     subnet_specs = self.subnet_specs(1)
-    #     result = ''
-    #     for index, spec in enumerate(specs):
-    #         result += f"{spec}: {subnet_specs[index]}\n"
-    #     return result
+        self.subnet_specs = self.__subnet_specs(1)
         
-    def subnet_specs(self, subnet_count):
+    def __subnet_specs(self, subnet_count):
         while subnet_count < len(range(self.subnet_count)):
-            return self.subnet_specs(subnet_count + subnet_count)
+            return self.__subnet_specs(subnet_count + subnet_count)
         data = self.subnet_table[str(subnet_count)]
         data.insert(0, subnet_count)
         return data
     
     def subnetworks(self):
-        subnets_ids = []
-        subnet_specs = self.subnet_specs(1)
-        host_bit = int(self.network_id.split(".")[-1])
-        for i in range(subnet_specs[0]):
-            net_id = '.'.join(self.network_id.split(".")[:-1])
-            subnets_ids.append(str(net_id + "." + str(host_bit)))
-            host_bit += subnet_specs[1]
-        return subnets_ids
+        subnets = {
+            "Network ID": [],
+            "Broadcast ID": [],
+            "Subnet Range": [],
+            "Subnet Mask": self.subnet_specs[2],
+            "Usable Hosts": self.subnet_specs[1] - 2
+        }
+        host_bit = 0
+        broad_id = self.subnet_specs[1] - 1
+        net_id = '.'.join(self.network_id.split(".")[:-1])
+        for i in range(self.subnet_specs[0]):
+            subnets["Network ID"].append(str(net_id + "." + str(host_bit)))
+            subnets["Broadcast ID"].append(str(net_id + "." + str(broad_id)))
+            subnets["Subnet Range"].append(str(net_id + "." + str(host_bit + 1) + "-" + str(net_id + "." + str(broad_id - 1))))
+            host_bit += self.subnet_specs[1]
+            broad_id += self.subnet_specs[1]
+        return subnets
         
         
 if __name__ == "__main__":
