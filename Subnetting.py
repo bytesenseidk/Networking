e06dbffdd8d8f7acd13b1b1f57d7cb2169229acc
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-13 12:08:54 +0200

    subnet specs

--- Subnetting.py
+++ Subnetting.py
@@ -9,3 +9,16 @@ class Subnet(object):
             "8": [32, 27], "16": [16, 28], "32": [8, 29],
             "64": [4, 30], "128": [2, 31], "256": [1, 32]
         }
+        self.subnet_specs = self.__subnet_specs(1)
+    
+    def __str__(self):
+        return str(tabulate(self.subnetworks(), headers="keys", tablefmt="github"))
+    
+    def __subnet_specs(self, subnet_count):
+        while subnet_count < len(range(self.subnet_count)):
+            return self.__subnet_specs(subnet_count + subnet_count)
+        data = self.subnet_table[str(subnet_count)]
+        data.insert(0, subnet_count)
+        return data
+    
+    
